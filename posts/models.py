9b3c5b2c32ce5fc93a24a582a98e21ce24536054
Julia <juliancamilohermida@hotmail.com>
2021-05-05 11:23:29 -0500

    Funcionalidad de comentar añadida.

--- posts/models.py
+++ posts/models.py
@@ -4,6 +4,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# Models
+from users.models import Profile
+
 
 class Post(models.Model):
     """Post model."""
@@ -20,3 +23,16 @@ class Post(models.Model):
     def __str__(self):
         """Return title and username."""
         return '{} by @{}'.format(self.title, self.user.username)
+
+
+class Comment(models.Model):
+    """Comment model."""
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True)
+    comment = models.TextField(blank=True)
+
+    def __str__(self):
+        """Return user and pk"""
+        return "{} by @{}".format(self.pk, self.user.username)
--- posts/views.py
+++ posts/views.py
@@ -1,15 +1,18 @@
 """Posts views."""
 
 # Django
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
 from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic.edit import DeleteView
 
 # Forms
-from posts.forms import PostForm
+from posts.forms import PostForm, CommentForm
 
 # Models
-from posts.models import Post
+from posts.models import Post, Comment
 
 
 class PostsFeedView(LoginRequiredMixin, ListView):
@@ -21,6 +24,12 @@ class PostsFeedView(LoginRequiredMixin, ListView):
     paginate_by = 30
     context_object_name = 'posts'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comments'] = Comment.objects.all()
+        context['form_comments'] = CommentForm()
+        return context
+
 
 class PostDetailView(LoginRequiredMixin, DetailView):
     """Return post detail."""
@@ -28,6 +37,13 @@ class PostDetailView(LoginRequiredMixin, DetailView):
     template_name = 'posts/detail.html'
     queryset = Post.objects.all()
     context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comments'] = Comment.objects.filter(post=self.get_object()).all()
+        context['form_comments'] = CommentForm()
+        return context
+
 
 
 class CreatePostView(LoginRequiredMixin, CreateView):
@@ -43,3 +59,38 @@ class CreatePostView(LoginRequiredMixin, CreateView):
         context['user'] = self.request.user
         context['profile'] = self.request.user.profile
         return context
+
+
+@login_required
+def save_comment(request):
+    """Save comments."""
+
+    if request.method == 'POST':
+
+        post = {
+            "user": request.user.id,
+            "profile": request.user.id,
+            "comment": request.POST["comment"],
+            "post": request.POST["post"]
+        }
+
+        form = CommentForm(post)
+
+        if form.is_valid():
+            form.save()
+            return redirect("posts:feed")
+    else:
+        return HttpResponse(status=405)
+    return HttpResponse(status=500)
+
+
+class DeletePostView(LoginRequiredMixin, DeleteView):
+    """Delete a post."""
+
+    template_name = "posts/delete.html"
+    model = Post
+    success_url = reverse_lazy("posts:feed")
+
+    def get_object(self):
+        obj = super().get_object()
+        obj.delete()
